113564102938b5390825fa67c1ae540769ea058a
Bibek Gyawali <bibekgyawali123@gmail.com>
2020-08-07 17:15:42 -0500

    footer and header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  return(
+    <div>
+          <Navbar/>
+          <h1>BIbek Gyawali</h1>
+          <Footer/>
     </div>
-  );
+  )
 }
-
-export default App;
