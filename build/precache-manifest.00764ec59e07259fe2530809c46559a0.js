4e724260b2dcec3c930e637f653ea5ba8f23ac0e
Bibek Gyawali <bibekgyawali123@gmail.com>
2020-08-08 11:58:50 -0500

    build

---
+++ build/precache-manifest.00764ec59e07259fe2530809c46559a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77fd6b720d53728dbf02c3b2a938b0cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3e68fa4bf950660217c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fc4b7b43023ed445f46d",
+    "url": "/static/js/2.0dcb4501.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0dcb4501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e68fa4bf950660217c",
+    "url": "/static/js/main.899e49f9.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
